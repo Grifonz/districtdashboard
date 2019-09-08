023fbc81b8e13ea2b8da6837396a56c04a7bdc54
Grifonz <52971253+Grifonz@users.noreply.github.com>
2019-09-08 23:12:18 +0200

    Add files via upload

---
+++ src/components/Chart.vue
@@ -0,0 +1,22 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+  extends: HorizontalBar,
+  props: ["chartData", "options"],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler() {
+        //update chart
+        this.$data._chart.update();
+      }
+    }
+  }
+};
+</script>
+<style lang="scss">
+</style>
